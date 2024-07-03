e9ddd80ade7c1558fa8556a5d3fd1476a2e2767a
Imunury <dipblu0808@gmail.com>
2024-07-03 17:29:48 +0900

    kakao_map delete

--- src/app/component/Kakaomap.tsx
+++ src/app/component/Kakaomap.tsx
@@ -1,32 +1,32 @@
-// components/KakaoMap.tsx
-import React, { useEffect } from 'react';
+// // components/KakaoMap.tsx
+// import React, { useEffect } from 'react';
 
-const KakaoMap: React.FC = () => {
-    useEffect(() => {
-        const initializeMap = () => {
-            if (window.kakao && window.kakao.maps) {
-                const container = document.getElementById('map');
-                const options = {
-                    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-                    level: 3,
-                };
-                new window.kakao.maps.Map(container, options);
-            }
-        };
+// const KakaoMap: React.FC = () => {
+//     useEffect(() => {
+//         const initializeMap = () => {
+//             if (window.kakao && window.kakao.maps) {
+//                 const container = document.getElementById('map');
+//                 const options = {
+//                     center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+//                     level: 3,
+//                 };
+//                 new window.kakao.maps.Map(container, options);
+//             }
+//         };
 
-        if (window.kakao && window.kakao.maps) {
-            window.kakao.maps.load(initializeMap);
-        } else {
-            const checkKakaoMap = setInterval(() => {
-                if (window.kakao && window.kakao.maps) {
-                    clearInterval(checkKakaoMap);
-                    initializeMap();
-                }
-            }, 100);
-        }
-    }, []);
+//         if (window.kakao && window.kakao.maps) {
+//             window.kakao.maps.load(initializeMap);
+//         } else {
+//             const checkKakaoMap = setInterval(() => {
+//                 if (window.kakao && window.kakao.maps) {
+//                     clearInterval(checkKakaoMap);
+//                     initializeMap();
+//                 }
+//             }, 100);
+//         }
+//     }, []);
 
-    return <div id="map" style={{ width: '100%', height: '500px', border: '1px solid black' }}></div>;
-};
+//     return <div id="map" style={{ width: '100%', height: '500px', border: '1px solid black' }}></div>;
+// };
 
-export default KakaoMap;
+// export default KakaoMap;
