8117f7eebeb8c4c6f5df00ff863a0c69e892d4e5
Imunury <dipblu0808@gmail.com>
2024-07-18 14:09:55 +0900

    rewrites

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "https://eco-dashboard-kappa.vercel.app",
+        },
+      ],
+    };
+  },
+};
