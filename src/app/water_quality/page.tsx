d8eb63737514ed873f5470add0e0b4077cba3014
Imunury <dipblu0808@gmail.com>
2024-08-02 10:40:29 +0900

    wq 주소

--- src/app/water_quality/page.tsx
+++ src/app/water_quality/page.tsx
@@ -1,8 +1,13 @@
 const WaterQuality: React.FC = () => {
     return (
-        <iframe className="w-full h-full" src="http://ecobotdashboard1.co.kr:3200/d/homepage_dashboard_wq/dashboard-wq?orgId=1&">
+        <div className="w-full h-full">
+            <iframe className="w-full h-1/2" src="http://ecobotdashboard1.co.kr:3200/d/homepage_dashboard_wq/dashboard-wq?orgId=1&">
 
-        </iframe>
+            </iframe>
+            <iframe className="w-full h-1/2" src="/grafana/d/homepage_dashboard_wq/dashboard-wq?orgId=1">
+
+            </iframe>
+        </div>
     )
 }
 
