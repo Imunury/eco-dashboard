5bc4fc94c507f7e0effc97544ab8041fef2ed3de
Imunury <dipblu0808@gmail.com>
2024-08-05 10:14:36 +0900

    http -> https

--- src/app/water_quality/page.tsx
+++ src/app/water_quality/page.tsx
@@ -1,7 +1,7 @@
 const WaterQuality: React.FC = () => {
     return (
         <div className="w-full h-full">
-            <iframe className="w-full h-1/2" src="http://ecobotdashboard1.co.kr:3200/d/homepage_dashboard_wq/dashboard-wq?orgId=1&">
+            <iframe className="w-full h-1/2" src="https://ecobotdashboard1.co.kr:3200/d/homepage_dashboard_wq/dashboard-wq?orgId=1&">
 
             </iframe>
             <iframe className="w-full h-1/2" src="/grafana/d/homepage_dashboard_wq/dashboard-wq?orgId=1">
