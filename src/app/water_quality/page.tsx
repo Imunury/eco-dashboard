beb6728a5ecd6e3da1f10cdf828a1bf58709fcac
Imunury <dipblu0808@gmail.com>
2024-08-05 10:18:53 +0900

    testtest

--- src/app/water_quality/page.tsx
+++ src/app/water_quality/page.tsx
@@ -4,9 +4,6 @@ const WaterQuality: React.FC = () => {
             <iframe className="w-full h-1/2" src="https://ecobotdashboard1.co.kr:3200/d/homepage_dashboard_wq/dashboard-wq?orgId=1&">
 
             </iframe>
-            <iframe className="w-full h-1/2" src="/grafana/d/homepage_dashboard_wq/dashboard-wq?orgId=1">
-
-            </iframe>
         </div>
     )
 }
