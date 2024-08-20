3d78a4fe2384e2b6f4ca457f40cc6af271040b83
Imunury <dipblu0808@gmail.com>
2024-08-20 14:05:44 +0900

    background color

--- src/app/components/RobotList.tsx
+++ src/app/components/RobotList.tsx
@@ -1,16 +1,22 @@
 'use client'
 
 import { ecobot_status_temp } from '@prisma/client';
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname, useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const RobotList: React.FC = () => {
     const router = useRouter();
     const pathname = usePathname() || '';
 
+    const params = useParams();
+    const id = params?.id as string | undefined
+
     const [robotList, setRobotList] = useState<ecobot_status_temp[]>([]);
+    const [selectedRobotId, setSelectedRobotId] = useState<string | undefined>(id);
 
     useEffect(() => {
+
+        setSelectedRobotId(id)
 
         const fetchData = async () => {
             try {
@@ -27,9 +33,10 @@ const RobotList: React.FC = () => {
         const intervalId = setInterval(fetchData, 5000);
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [params]);
 
     const handleRobotClick = (robotId: string) => {
+        setSelectedRobotId(robotId);
         if (pathname.startsWith('/control')) {
             router.push(`/control/${robotId}`);
         } else if (pathname.startsWith('/tracking_map')) {
@@ -48,7 +55,10 @@ const RobotList: React.FC = () => {
                         const isMotorOn = firstValue === 1;
 
                         return (
-                            <li key={index} onClick={() => handleRobotClick(data.robot_id)} className='p-3 cursor-pointer border-solid border-black border-b'>
+                            <li
+                                key={data.robot_id}
+                                onClick={() => handleRobotClick(data.robot_id)}
+                                className={`p-3 cursor-pointer border-solid border-black border-b transition-colors duration-500 ${selectedRobotId === data.robot_id ? 'bg-yellow-300' : ''}`}>
                                 <p>로봇 아이디 : {data.robot_id}</p>
                                 <p>ON / OFF : {firstValue !== null ? (isMotorOn ? "ON" : "OFF") : "NO DATA"}</p>
                                 <p>배터리(%) : {data.ctr_bat_soc !== null ? data.ctr_bat_soc : "NO DATA"}</p>
